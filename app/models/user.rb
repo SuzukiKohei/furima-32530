8eeeaaffeeae8d21ae64a0d3f8a70f20e42e8f8b
SuzukiKohei <160781146@ccalumni.meijo-u.ac.jp>
2021-01-07 18:58:30 +0900

    バリデーションをかけ、rspecを導入した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
         validates :nickname,           presence: true
-        validates :first_name,         presence: true
-        validates :last_name,          presence: true
-        validates :first_name_reading, presence: true
-        validates :last_name_reading,  presence: true
+
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カナ文字を使用してください' } do
+          validates :first_name_reading
+          validates :last_name_reading
+        end
         validates :birthday,           presence: true
 end
