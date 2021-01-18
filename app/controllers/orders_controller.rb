17c69135fb07ee1f6425ef0cb7df4f355be19eaf
SuzukiKohei <160781146@ccalumni.meijo-u.ac.jp>
2021-01-18 16:35:07 +0900

    商品購入機能の修正４

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :sold_item, only: [:index, :create]
   before_action :not_same_user, only: [:index, :create]
 
   def index
@@ -27,6 +28,12 @@ class OrdersController < ApplicationController
 
   def purchase_history_address_params
     params.require(:purchase_history_address).permit(:postal_code, :shipping_prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def sold_item
+    if @item.purchase_history != nil
+      redirect_to root_path
+    end
   end
 
   def not_same_user
