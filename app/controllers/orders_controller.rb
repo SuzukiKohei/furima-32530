6a04d274a8f5bc6639f91157d47e89ac9e46e06c
SuzukiKohei <160781146@ccalumni.meijo-u.ac.jp>
2021-01-18 16:45:23 +0900

    商品購入機能の修正５

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
   before_action :sold_item, only: [:index, :create]
-  before_action :not_same_user, only: [:index, :create]
 
   def index
     @purchase_history_address = PurchaseHistoryAddress.new
@@ -31,13 +30,7 @@ class OrdersController < ApplicationController
   end
 
   def sold_item
-    if @item.purchase_history != nil
-      redirect_to root_path
-    end
-  end
-
-  def not_same_user
-    if current_user.id == @item.user_id
+    if @item.purchase_history != nil || current_user.id == @item.user_id 
       redirect_to root_path
     end
   end
